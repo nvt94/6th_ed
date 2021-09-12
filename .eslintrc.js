bb754ce2728ea588fc0274f693a52d74f4bb830e
nvt94 <nvt94.ptit@gmail.com>
2021-09-13 03:27:05 +0900

    pretier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    commonjs: true,
+    jquery: true
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
